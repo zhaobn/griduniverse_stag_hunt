7a4a67ed6bbe5f9cb876cbbc7c593fff9257c25c
bonan <zbn.dale@gmail.com>
2024-08-15 18:04:40 +0800

    Analysis

--- analysis/plot_location.R
+++ analysis/plot_location.R
@@ -1,27 +1,46 @@
 
 library(dplyr)
 library(tidyr)
+library(lubridate)
 library(ggplot2)
 
+pos = read.csv('start_distance.csv')
 
-data = read.csv('start_distance.csv')
+action = read.csv('all_cleaned.csv')
+action$time_stamp <- ymd_hms(action$time_stamp)
 
-# for each round, scale player position as -10 (closest to hare) to 10 (closest to stag)
-df_dist = data %>%
-  filter(round_id==5) %>%
+first_action = action %>%
+  filter(action=='item') %>%
+  select(game_id, round_id, player_id, value, time_stamp) %>%
+  group_by(game_id, round_id, player_id) %>%
+  slice_min(time_stamp) %>%
+  select(-time_stamp) %>%
+  mutate(player_id=paste0('player',player_id))
+
+
+df_dist = pos %>%
+  #filter(round_id==5) %>%
   mutate(to_hare = ifelse(to_hare1 < to_hare2, to_hare1, to_hare2)) %>%
   mutate(distance = (to_stag1-to_hare)/28) %>%
   select(round_id, game_id, player_id, distance)
+
+df_action = df_dist %>%
+  left_join(first_action, by=c('game_id', 'round_id', 'player_id')) %>%
+  mutate(action=ifelse(is.na(value), 'stag',  value)) %>%
+  select(game_id, round_id, player_id, action)
+
+df_info = action %>%
+  select(game_id, round_id, others_visible, chat_visible) %>%
+  unique()
+
 df_dist_long <- df_dist %>%
   pivot_wider(names_from = player_id, values_from = distance)
 
-df_info = data %>%
-  select(round_id, game_id, action, chat_visible, others_visible) %>%
-  unique() %>%
-  mutate(coop=as.numeric(action=='stag'))
 
 df_extended = df_dist_long %>%
-  left_join(df_info, by=c('round_id', 'game_id'))
+  left_join(df_info, by=c('round_id', 'game_id')) %>%
+  full_join(df_action, by=c('round_id', 'game_id')) %>%
+  mutate(coop=as.numeric(action=='stag'))
 
 # make scatter plot
 ggplot(df_extended, aes(x = player1, y = player2)) +
@@ -36,19 +55,4 @@ ggplot(df_extended, aes(x = player1, y = player2)) +
   theme_bw() +
   facet_grid(others_visible~chat_visible, labeller = label_both) +
   theme(text = element_text(size = 20))
-
-
-
-
-
-# Santity check for the other plot
-df_type = data %>%
-  mutate(dist_type = ifelse(to_stag1 < to_hare1 & to_stag1 < to_hare2, 'stag_closest', 
-                ifelse(to_stag1 > to_hare1 & to_stag1 > to_hare2, 'stag_furthemost', 'stag_between')),
-         coop = as.numeric(action=='stag')) %>%
-  group_by(game_id, round_id, chat_visible, others_visible, dist_type) %>%
-  summarise(coop=min(coop))
-
-
-
 
